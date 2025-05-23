bcada069c2835724327489fd94a999fb920c9b60
AlexSuvak94 <alexwriting94@gmail.com>
2025-05-23 17:40:43 +0200

    finally some progress

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CarController;
 
 Route::get('/', function () {
-    return view('welcome'); // The main view
+    return view('cars-view'); // The main view
 });
 
 Route::post('/add-car', [CarController::class, 'store']); // This handles creation of a new item (car)
