d4bd751c7d6f6cf96a3e57e8160e36167bc4fcd4
AlexSuvak94 <alexwriting94@gmail.com>
2025-05-23 20:50:55 +0200

    sdfdfdfd

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Schema::defaultStringLength(191);
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
